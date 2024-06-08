aee4e4969cf72e12cb075a8c9466e044dd2c0434
Lanbert Ry V. Tecson <110764429+Lantecs@users.noreply.github.com>
2024-06-08 15:33:35 +0800

    Update utils.js

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -5,7 +5,7 @@ setRouter();
 
 // Backend URL
 const backendURL =
-  "https://2c8d-180-194-3-43.ngrok-free.app/backend-techcare/public";
+  "https://b184-216-247-56-68.ngrok-free.app/backend-techcare-main/public";
 
 // Get Logged User Profile
 async function getLoggedUser() {
