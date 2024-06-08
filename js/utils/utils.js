6489a25b65bfdb23c076277a98c3acdd52ce5094
Rose Baqueran <baqueranrose@gmail.com>
2024-06-08 20:49:36 +0800

    update

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -5,7 +5,7 @@ setRouter();
 
 // Backend URL
 const backendURL =
-  "https://edf1-216-247-56-68.ngrok-free.app/backend-techcare-main/public";
+  "https://bee3-216-247-56-68.ngrok-free.app/backend-techcare-main/public";
 
 // Get Logged User profile
 async function getLoggedUser() {
